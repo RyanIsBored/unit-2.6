244804376113f2af1c249e558468a4da0c111ee0
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-02-08 12:19:11 -0500

    Updated from Brython Server: 2/8/2018 12:19:11 PM

--- house.py
+++ house.py
@@ -1,1 +1,17 @@
+#Ryan Jones
+#2/8/18
 
+from ggame import *
+
+red = Color(0xFF0000,1)
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+yellow = Color(0xFFFF00,1)
+
+
+yellowRectangle = RectangleAsset(200,200,yellow)
+
+Sprite(yellowRectangle)
+
+App().run()
