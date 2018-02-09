cbaff862b81b17bed1d0206ae77500936f606353
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-02-09 08:16:54 -0500

    Updated from Brython Server: 2/9/2018 8:16:53 AM

--- house.py
+++ house.py
@@ -8,9 +8,10 @@ green = Color(0x00FF00,1)
 blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
 yellow = Color(0xFFFF00,1)
+noOutline = LineStyle(0,black)
 
 
-yellowRectangle = RectangleAsset(200,200,yellow)
+yellowRectangle = RectangleAsset(200,200,noOutline,yellow)
 
 Sprite(yellowRectangle)
 
