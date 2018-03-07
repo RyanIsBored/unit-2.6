9c60858ab3095d0b47586332dd114608bf485c2d
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-03-07 08:37:07 -0500

    Updated from Brython Server: 3/7/2018 8:37:06 AM

--- house.py
+++ house.py
@@ -12,15 +12,16 @@ yellow = Color(0xFFFF00,1)
 noOutline = LineStyle(0,black)
 
 
-yellowRectangle = RectangleAsset(300,300,noOutline,yellow)
+greenRectangle = RectangleAsset(300,300,noOutline,green)
 redTriangle = PolygonAsset([(200,200), (350,100), (500,200)],noOutline,red)
 blackRectangle = RectangleAsset(100,100,noOutline,black)
 text = TextAsset('house',fill=blue, style='bold 40pt wingdings')
+yellowCircle = CircleAsset(10,noOutline,yellow)
 
 
-Sprite(yellowRectangle,(200,200))
+Sprite(greenRectangle,(200,200))
 Sprite(redTriangle,(200,100))
 Sprite(blackRectangle,(300,400))
 Sprite(text,(250,300))
-
+Sprite(yellowCircle,(305,440))
 App().run()
